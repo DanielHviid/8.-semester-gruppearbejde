8d88fa951c1a2902f1308270deb3bdbb74bbfac9
Javakin <danielharaldson@yahoo.com>
2018-03-04 17:53:59 +0100

    stuff er lavet

--- MachineLearning/Aflevering_2/Opgave1_1.R
+++ MachineLearning/Aflevering_2/Opgave1_1.R
@@ -1,12 +1,26 @@
 
+#--------------------------------------------
+#--------------- Opgave 2.1.1.1 del 1
+#--------------------------------------------
+
+
+
 ## signs are random
 require(graphics)
 
 ## the variances of the variables in the
 ## USArrests data vary by orders of magnitude, so scaling is appropriate
 prcomp(USArrests)  # inappropriate
-prcomp(USArrests, scale = TRUE)
+prcomp(USArrests, scale = TRUE, center = TRUE)
 prcomp(~ Murder + Assault + Rape, data = USArrests, scale = TRUE)
 plot(prcomp(USArrests))
 summary(prcomp(USArrests, scale = TRUE))
 biplot(prcomp(USArrests, scale = TRUE))
+
+
+
+
+
+#--------------------------------------------
+#--------------- Opgave 2.1.1.1 del 2
+#--------------------------------------------
--- MachineLearning/Aflevering_2/main.R
+++ MachineLearning/Aflevering_2/main.R
@@ -1,7 +1,7 @@
 
 
 #setup paths for the mashine in use
-source('Aflevering_2/Setup_Haraldson.R', chdir = TRUE)
+#source('Aflevering_2/Setup_Haraldson.R', chdir = TRUE)
 
 #load relevant functions
 source(paste(ProjectLocation, 'loadImage.R', sep=""))
@@ -9,4 +9,16 @@ source(paste(ProjectLocation, 'functions.R', sep=""))
 
 
 # Perform exercises
+source(paste(ProjectLocation, 'Opgave1_1.R', sep=""))
+source(paste(ProjectLocation, 'Opgave1_2.R', sep=""))
+source(paste(ProjectLocation, 'Opgave1_3.R', sep=""))
 
+
+source(paste(ProjectLocation, 'Opgave2_1.R', sep=""))
+
+
+source(paste(ProjectLocation, 'Opgave3_1.R', sep=""))
+source(paste(ProjectLocation, 'Opgave3_2.R', sep=""))
+source(paste(ProjectLocation, 'Opgave3_3.R', sep=""))
+source(paste(ProjectLocation, 'Opgave3_4.R', sep=""))
+source(paste(ProjectLocation, 'Opgave3_5.R', sep=""))
