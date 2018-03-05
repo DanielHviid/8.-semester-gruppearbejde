fbcf9e68fe16f36a256c611f34a5e984ebfe759e
Javakin <danielharaldson@yahoo.com>
2018-03-06 00:30:06 +0100

    pass

--- MachineLearning/Aflevering_2/Opgave1_1.R
+++ MachineLearning/Aflevering_2/Opgave1_1.R
@@ -3,7 +3,11 @@
 #--------------- Opgave 2.1.1.1 del 1
 #--------------------------------------------
 
+res <- prcomp(id100Small)
+summary(res)
 
+# giver åbenbart et andet resultat
+summary(prcomp(id200Small, scale = TRUE))
 
 ## signs are random
 require(graphics)
@@ -14,7 +18,7 @@ prcomp(USArrests)  # inappropriate
 prcomp(USArrests, scale = TRUE, center = TRUE)
 prcomp(~ Murder + Assault + Rape, data = USArrests, scale = TRUE)
 plot(prcomp(USArrests))
-summary(prcomp(USArrests, scale = TRUE))
+summary(prcomp(id100Small, scale = TRUE))
 biplot(prcomp(USArrests, scale = TRUE))
 
 
