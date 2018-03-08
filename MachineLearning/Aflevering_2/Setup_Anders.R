284a3bd9ce9a8e2da565cacddd6b91e3a3c1a852
Anders Ellinge <andersellinge.ae@gmail.com>
2018-03-08 15:52:34 +0100

    setup anders

---
+++ MachineLearning/Aflevering_2/Setup_Anders.R
@@ -0,0 +1,9 @@
+# ------------------------------- Ubuntu (laptop)  ------------------------------------------ 
+
+
+ProjectLocation <- '/home/anders/Desktop/workspace/8.-semester-gruppearbejde/MachineLearning/Aflevering_2/'
+setwd(ProjectLocation)
+
+folder <- "/home/anders/Desktop/workspace/STML/trunk/preProcessed/2018/group"
+
+
